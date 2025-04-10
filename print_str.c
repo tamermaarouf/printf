8fec3a07065ad5c339979d07b3235066917a90c5
Tamer_Ma3rouf <tamermaarouf@gmail.com>
2025-04-10 14:14:39 +0200

    Fix

--- print_str.c
+++ print_str.c
@@ -2,27 +2,10 @@
 
 int print_str(va_list arg)
 {
-	int count, i;
+	int count;
        	char *str;
 
 	count = 0;
-	str = va_arg(arg, char *);
-	if (str == NULL)
-	{
-		str = "(null)";
-		count = _strlen(str);
-		for (i = 0; i < count; i++)
-			_putchar(str[i]);
-		return (count);
-	}
-	else
-	{
-		count = _strlen(str);
-		for (i = 0; i < count; i++)
-			_putchar(str[i]);
-		return (count);
-	}
-	/*
 	while (*str)
 	{
 		print_char(arg);
@@ -30,5 +13,4 @@ int print_str(va_list arg)
 		++str;
 	}
 	return count;
-	*/
 }
