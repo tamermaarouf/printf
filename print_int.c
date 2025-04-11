3a174c02e736c69a7c8f6239a4cc8562236ed947
Tamer_Ma3rouf <tamermaarouf@gmail.com>
2025-04-11 05:48:34 +0200

    Fix

--- print_int.c
+++ print_int.c
@@ -4,8 +4,19 @@ int print_int(va_list arg)
 {
 	int count, n;
 	count = 0;
+
 	n = va_arg(arg, int);
-	if (n < 10)
-		_putchar(n);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while ((n / 10) != 0)
+	{
+		_putchar((n % 10) + 48);
+		n = n / 10;
+		count++;
+	}
+	_putchar((n % 10) + 48);
 	return (count);
 }
