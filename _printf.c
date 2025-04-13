1cbece978b95c3521a5c9f01b7cbf263b5ea475b
Tamer_Ma3rouf <tamermaarouf@gmail.com>
2025-04-13 20:01:48 +0200

    create print buffer

--- _printf.c
+++ _printf.c
@@ -10,26 +10,33 @@ int _printf(const char *format, ...)
 {
 	va_list ap;
 	int (*f)(va_list);
-	int count;
+	int count, index;
 
 	if (format == NULL)
 		return (-1);
 	va_start(ap, format);
 	count = 0;
-	while (*format != '\0')
+	index = 0;
+	while (*(format + index) != '\0')
 	{
-		if (*format == '\0')
+		if (*(format + index) == '\0')
 			return (count);
-		if (*format == '%')
+		if (*(format + index) == '%')
 		{
-			f = get_op_func(++format);
-			if (f != NULL)
-				count += f(ap);
+			index++;
+			if (*(format + index) == '%')
+				_putchar('%');
+			else
+			{
+				f = get_op_func(&format[index]);
+				if (f != NULL)
+					count += f(ap);
+			}
 
 		}
 		else
-			count += write(1, format, 1);
-		format++;
+			count += write(1, &format[index], 1);
+		index++;
 	}
 	va_end(ap);
 	return (count);
