28fde9a66d00d87976e2c5ce7d8764a6b9d5bc4c
Tamer_Ma3rouf <tamermaarouf@gmail.com>
2025-04-24 12:24:54 +0200

    specifier d & i

--- print_dec.c
+++ print_dec.c
@@ -9,23 +9,33 @@
 
 int print_dec(t_data *data, va_list arg)
 {
-	int N, i;
+	int N, i, x;
 
 	N = va_arg(arg, int);
+	x = N;
 	data->buffer_digit = 0;
-	while (N)
+
+	if (x < 0)
 	{
-		/**
-		 * Separate last digit from
-		 * the number and add ASCII
-		 * value of character '0' is 48
-		 */
-		data->buf_num[data->buffer_digit++] = ((N % 10) + 48);
-		/**
-		 * Truncate the last
-		 * digit from the number
-		 */
-		N /= 10;
+		data->buff[data->buffer_index++] = 0x2D;
+		x = -N;
+	}
+	if (x > 0)
+	{
+		while (x)
+		{
+			/**
+			 * Separate last digit from
+			 * the number and add ASCII
+			 * value of character '0' is 48
+			 */
+			data->buf_num[data->buffer_digit++] = ((x % 10) + 48);
+			/**
+			 * Truncate the last
+			 * digit from the number
+			 */
+			x /= 10;
+		}
 	}
 	for (i = data->buffer_digit; i >= 0; i--)
 		data->buff[data->buffer_index++] = data->buf_num[i];
--- print_int.c
+++ print_int.c
@@ -12,20 +12,27 @@ int print_int(t_data *data, va_list arg)
 
 	N = va_arg(arg, int);
 	data->buffer_digit = 0;
-	while (N)
+	if (N < 0)
 	{
-		/**
-		 * Separate last digit from
-		 * the number and add ASCII
-		 * value of character '0' is 48
-		 */
-
-		data->buf_num[data->buffer_digit++] = ((N % 10) + 48);
-		/**
-		 * Truncate the last
-		 * digit from the number
-		 */
-		N /= 10;
+		data->buf_num[data->buffer_digit++] = '-';
+		N = -N;
+	}
+	else
+	{
+		while (N)
+		{
+			/**
+			 * Separate last digit from
+			 * the number and add ASCII
+			 * value of character '0' is 48
+			 */
+			data->buf_num[data->buffer_digit++] = ((N % 10) + 48);
+			/**
+			 * Truncate the last
+			 * digit from the number
+			 */
+			N /= 10;
+		}
 	}
 	for (i = data->buffer_digit; i >= 0; i--)
 		data->buff[data->buffer_index++] = data->buf_num[i];
